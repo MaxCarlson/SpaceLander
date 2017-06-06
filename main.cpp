099181f28119ba0931c0ad87a2f29e69d0622429
Maxwell Carlson <bart4282819@gmail.com>
2017-06-05 21:51:10 -0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: C-60
+ *
+ * Created on June 5, 2017, 9:49 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+
+class Lander{
+public:
+    int time = 0, height = 200000, speed = 1600 , fuel = 7500;
+    double gravity = 1.30;
+    
+    Lander(){
+        
+        
+    }
+
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
