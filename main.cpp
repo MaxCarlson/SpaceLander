71ef895347d7cd9765b85b1e76d60814023bc9b7
Maxwell Carlson <bart4282819@gmail.com>
2017-06-05 23:49:43 -0700

    mass, dv, height, speed, gravity equations

--- main.cpp
+++ main.cpp
@@ -27,10 +27,15 @@ using namespace std;
 
 class Lander{
 public:
-    //Landing variables set to moon defaults
-    int time = 1, height = 200000, speed = 1600 , fuel = 7500;
-    double gravity = 1.30;
+    //Planet variables set to moon defaults
+    long time = 0, height = 200000, radius = 1737100;
+    double gravity = 1.304, g0gravity = 1.622;
     string location = "Moon";
+    //Rocket variables
+    int fuel = 8200, throttle = 0, rMass = 2134, velocity = 1600;
+    double deltaV = 0, deltaMass = 0, Vexhaust = 2800;
+    //Time increments
+    double dt = 0.1;
     
     Lander(){
         cout << "Where would you like to attempt a landing? Enter one: Moon," << endl;
@@ -45,20 +50,36 @@ public:
             cout << "You are to perform manual landing by controlling engines" << endl;
             cout << "specify fuel burning rate (kgs per second) for each 10 sec " << endl;
             cout << "and try to touch down with safe speed. Good luck!!!" << endl;
-            cout << "Rocket weight: " << fuel << " kg" << endl;
+            cout << "Rocket weight: " << rMass+fuel << " kg" << endl;
         }
     }
     
     void printGame(){
         stringstream timetmp, heighttmp, speedtmp, fueltmp;
-        timetmp << time; heighttmp << height; speedtmp << speed; fueltmp << fuel;
+        timetmp << time; heighttmp << height; speedtmp << velocity; fueltmp << fuel;
         cout << "Time" << "   Height(m)" << "   Speed(m/s)" << "   Fuel(kg)" << "   Gravity(m/s^2)" << endl;
-        cout << setw((4-timetmp.gcount())) << time; 
+        cout << setw(4-timetmp.gcount()) << time; 
         cout << setw(11-heighttmp.gcount())<< height; 
-        cout << setw(11-speedtmp.gcount()) << speed;
+        cout << setw(11-speedtmp.gcount()) << velocity;
         cout << setw(11-fueltmp.gcount()) << fuel;
-        cout << setw(14) << gravity << endl;
+        cout << setw(14) << setprecision(2) << gravity << endl;
+        cout << "Burning rate: "; 
+        cin >> throttle;
+        
     }
+    
+    void numericIntegration(){
+        //Lander height, velocity, and mass changes
+        height = height - velocity*dt;
+        deltaMass = dt*throttle;
+        deltaV = Vexhaust * deltaMass/(rMass+fuel);
+        fuel -= deltaMass;
+        //Gravity acceleration
+        gravity = g0gravity * (radius*radius) / ((radius+height)*(radius+height));
+        
+    }
+    
+    
 };
 
 int main(int argc, char** argv) {
