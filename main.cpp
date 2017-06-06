6311beb0a71a8857f3b1f5fd8f2274f99513165a
Maxwell Carlson <bart4282819@gmail.com>
2017-06-05 22:23:37 -0700

    inputs

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@
  */
 
 #include <cstdlib>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -21,17 +25,40 @@ using namespace std;
 
 class Lander{
 public:
+    //Landing variables set to moon defaults
     int time = 0, height = 200000, speed = 1600 , fuel = 7500;
     double gravity = 1.30;
+    string location = "Moon";
     
     Lander(){
-        
+        cout << "Where would you like to attempt a landing? Enter one: Moon," << endl;
+        cin >> location;
         
     }
-
+    
+    void setLocation(){
+        if(location == "moon" || "Moon"){
+            cout << "You are in a rocket approaching the Moon!" << endl;
+            cout << "Th100e main computer failed, your pilot's unconscious!" << endl;
+            cout << "You are to perform manual landing by controlling engines" << endl;
+            cout << "specify fuel burning rate (kgs per second) for each 10 sec " << endl;
+            cout << "and try to touch down with safe speed. Good luck!!!" << endl;
+            cout << "Rocket weight: " << fuel << " kg" << endl;
+        }
+    }
+    
+    void printGame(){
+        cout << "Time" << "   Height(m)" << "   Speed(m/s)" << "   Fuel(kg)" << "   Gravity(m/s^2)" << endl;
+        
+    }
+};
 
 int main(int argc, char** argv) {
 
+    Lander newLanding;
+    newLanding.setLocation();
+    newLanding.printGame();
+    
     return 0;
 }
 
